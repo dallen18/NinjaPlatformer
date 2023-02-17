038bd684b57dfca2b169da659ecc216eda55c359
AdamShk123 <adamshkolnik5@gmail.com>
2023-02-16 20:19:59 -0600

    header file for Game.cpp

---
+++ src/headers/Game.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Player.hpp"
+
+class Game
+{
+    private:
+        
+    public:
+        sf::RenderWindow window;
+        Game();
+        ~Game();
+        void run();
+        void initWindow();
+};
