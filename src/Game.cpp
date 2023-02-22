592e9502aa83917fd76060fdf9152bcda56115b1
AdamShk123 <adamshkolnik5@gmail.com>
2023-02-21 20:04:58 -0600

    moved movement logic to player object. added blocks to draw on screen

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,15 @@
 #include "headers/Game.hpp"
+#include <stdarg.h>
+#include <iostream>
+#include <vector>
 
 Game::Game()
 {
     //initializes window
     initWindow();
+
+    //loads textures
+    loadTextures();
 
     //runs program
     run();
@@ -23,6 +29,11 @@ void Game::initWindow()
     window.setVerticalSyncEnabled(true);
 }
 
+void Game::loadTextures()
+{
+
+}
+
 void Game::run()
 {
     sf::Texture playerTexture;
@@ -33,15 +44,45 @@ void Game::run()
         std::cout << "failed to load image.";
     }
 
+    //instantiates player with given texture
     Player player(&playerTexture);
 
     //make texture edges smoother if resolution is low
     //texture.setSmooth(true);
 
     //gets window size and set position of player sprite to middle of window
-    sf::Vector2u size = window.getSize();
+    sf::Vector2f size = (sf::Vector2f)window.getSize();
     sf::Sprite *playerSprite = player.getSprite();
-    playerSprite->setPosition(size.x/2,size.y/2);
+    playerSprite->setPosition(size.x/2 - playerTexture.getSize().x/2, size.y/2 - playerTexture.getSize().y);
+
+    float rectSize = 50.0f;
+
+    //list to keep track of all objects that player can collide with
+    std::vector<sf::RectangleShape> blockList;
+
+    //creates line of blocks across the screen
+    for(int i = 0; i < size.x; i += rectSize)
+    {
+        sf::RectangleShape r(sf::Vector2f(rectSize,rectSize));
+        r.setFillColor(sf::Color::Red);
+        r.setPosition(i,size.y/2);
+
+        blockList.push_back(r);
+    }
+
+    //random block
+    sf::RectangleShape r(sf::Vector2f(rectSize,rectSize));
+    r.setFillColor(sf::Color::Red);
+    r.setPosition(900,size.y/2 - 150);
+
+    blockList.push_back(r);
+
+    //random block
+    sf::RectangleShape f(sf::Vector2f(rectSize,rectSize));
+    f.setFillColor(sf::Color::Red);
+    f.setPosition(1200,size.y/2 - 50);
+
+    blockList.push_back(f);
 
     while (window.isOpen())
     {
@@ -56,30 +97,19 @@ void Game::run()
             }
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-        {
-            playerSprite->move(0.0f,-player.getVelocity());
-        }
+        player.move(&blockList);
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        {
-            playerSprite->move(0.0f,player.getVelocity());
-        }   
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        {
-            playerSprite->move(-player.getVelocity(),0.0f);
-        }   
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        {
-            playerSprite->move(player.getVelocity(),0.0f);
-        }   
-                
         window.clear();
+        for(sf::RectangleShape b: blockList)
+        {
+            window.draw(b);
+        }
         window.draw(*playerSprite);
         window.display();
     }
+}
 
-    //std::cout << "Hello World!";
+void Game::draw(int n, ...)
+{
+    //look at variadic templates c++ to add however many elements needed to draw and then calling it in run()
 }
