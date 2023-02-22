4e2aa54f8062e8959c3321517ef0f17325d283f3
AdamShk123 <adamshkolnik5@gmail.com>
2023-02-21 20:06:12 -0600

    added functions and variables to header file

--- src/headers/Player.hpp
+++ src/headers/Player.hpp
@@ -5,13 +5,24 @@ class Player
     private:
         sf::Texture t;
         sf::Sprite s;
-        float v;
+        float xVel;
+        float yVel;
+        bool bottom;
+        sf::FloatRect playerBounds;
+        sf::FloatRect nextBounds;
+        float xMax;
+        float yMax;
+        float accel;
 
     public:
         Player(sf::Texture *_t);
         ~Player();
         sf::Sprite *getSprite();
-        float getVelocity();
+        float getXVelocity();
+        float getYVelocity();
         void setTexture(sf::Texture *_t);
-        void setVelocity(float v);
+        void move(std::vector<sf::RectangleShape> *blockList);
+        void collision(std::vector<sf::RectangleShape> *blockList);
+        void setXVelocity(float v);
+        void setYVelocity(float v);
 };
