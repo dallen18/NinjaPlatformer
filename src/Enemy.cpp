7b59ae075c1bf5bcf898140feff634e15dc95b0a
AdamShk123 <adamshkolnik5@gmail.com>
2023-03-01 16:30:12 -0600

    Enemy class inherits from Entity

---
+++ src/Enemy.cpp
@@ -0,0 +1,29 @@
+#include "headers/Enemy.hpp"
+
+/*
+Constructor of Player class. Takes the following Parameters:
+    -std::vector<sf::Texture> *textures: passes textures that will be used by player
+    -float xMax: sets max speed in the x-axis for player
+    -float yMax: sets max speed in the y-axis for player
+    -float accel: sets acceleration rate for player
+
+*/
+Enemy::Enemy(std::vector<sf::Texture> *textures, float xMax, float yMax, float accel)
+{   
+    setTextures(textures);
+    getSprite()->setTexture(textures->front()); //sets initial texture of sprite as the first texture in list
+    setXVel(0.0f);
+    setYVel(0.0f);
+    setXMax(xMax);
+    setYMax(yMax);
+    setAccel(accel);
+}
+
+/*
+Method is responsible for animating the player sprite. It uses the textures passed in the constructor.
+It should have different animation for when character is at a stand-still compared to when it is moving.
+*/
+void Enemy::animation()
+{
+
+}
