7316b26ff455762085caedb977d90032cef4145c
AdamShk123 <adamshkolnik5@gmail.com>
2023-04-04 22:17:45 -0500

    added NPC class that receives string to use for pop up message

---
+++ src/NPC.cpp
@@ -0,0 +1,43 @@
+#include "headers/NPC.hpp"
+
+NPC::NPC(std::vector<sf::Texture> *textures, float xMax, float yMax, float accel, int xSize, int ySize, std::string m)
+{
+    setTextures(textures);
+    getSprite()->setTexture(textures->front()); //sets initial texture of sprite as the first texture in list
+    setXVel(0.0f);
+    setYVel(0.0f);
+    setXMax(xMax);
+    setYMax(yMax);
+    setAccel(accel);
+    setXSize(xSize);
+    setYSize(ySize);
+
+    for(int i = 1; i < m.size() + 1; i++)
+    {
+        message.push_back(m.at(i - 1));
+        if(i % 20 == 0)
+        {
+            message.push_back('\n');
+        }
+    }
+}
+
+std::string NPC::getString()
+{
+    return message;
+}
+
+std::string NPC::getClass()
+{
+    return "NPC";
+}
+
+void NPC::move()
+{
+
+}
+
+void NPC::animation()
+{
+
+}
