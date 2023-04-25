c2ca48318c8e304ce1e54f72df500749b06e894d
dallen18 <69290063+dallen18@users.noreply.github.com>
2023-04-25 11:53:32 -0500

    addition of sounds, sprite files and player code

---
+++ build/CMakeFiles/NinjaPlatformer.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NinjaPlatformer.dir/src/Block.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Block.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Button.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Button.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Enemy.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Enemy.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Entity.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Entity.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Game.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Game.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Hud.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Hud.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/InputManager.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/InputManager.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Main.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Main.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Menu.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Menu.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/NPC.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/NPC.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/Player.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/Player.cpp.o.d"
+  "CMakeFiles/NinjaPlatformer.dir/src/tinyxml2.cpp.o"
+  "CMakeFiles/NinjaPlatformer.dir/src/tinyxml2.cpp.o.d"
+  "NinjaPlatformer"
+  "NinjaPlatformer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NinjaPlatformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
